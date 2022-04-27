77a959395b163990c94128feb4bf7a56d2d5adf3
myjungwu <baek.myungsuk@gmail.com>
2022-04-27 14:56:44 +0900

    validation

--- src/main/java/com/basic/myspringboot/controller/form/UserForm.java
+++ src/main/java/com/basic/myspringboot/controller/form/UserForm.java
@@ -5,12 +5,11 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class UserForm {
-    @NotEmpty(message = "이름은 필수입력 항목입니다!")
+    @NotBlank(message = "이름은 필수입력 항목입니다!")
     private String name;
 
     @NotBlank(message = "E메일은 필수입력 항목입니다!")
