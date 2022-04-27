8ccd19471d20527d9fb193ccb4b85b002b7a5583
myjungwu <baek.myungsuk@gmail.com>
2022-04-27 11:48:34 +0900

    validation

--- src/main/java/com/basic/myspringboot/entity/User.java
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Getter @Setter
@@ -12,9 +15,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "이름은 필수입력 항목입니다!")
     @Column
     private String name;
 
+    @NotBlank(message = "E메일은 필수입력 항목입니다!")
+    @Email(message = "올바른 E메일 형식이 아닙니다!")
     @Column(unique=true)
     private String email;
 }
